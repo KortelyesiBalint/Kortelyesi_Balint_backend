62433c790adec22b4d841bbd09e79c3cf8e56ff7
KortelyesiBalint <balintkortelyesi@gmail.com>
2025-04-02 10:57:40 +0200

    asd2

--- app/Http/Controllers/IngatlanController.php
+++ app/Http/Controllers/IngatlanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Ingatlan;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class IngatlanController extends Controller
 {
@@ -15,6 +16,22 @@ class IngatlanController extends Controller
 
     public function store(Request $request)
     {
-        
+        try {
+            $request->validate([
+                "kategoria"=> "required|integer|exists:kategorias,id",
+                "leiras"=> "required|string",
+                "hirdetesDatuma"=> "required|date",
+                "tehermentes"=> "required|boolean",
+                "ar"=> "required|integer|max:50",
+                "kepUrl"=> "required|string",
+            ]);
+
+            $ingatlan = Ingatlan::create($request->all());
+            return response()->json(["id" => $ingatlan->id], 201, options:JSON_UNESCAPED_UNICODE);
+
+        } catch (ValidationException $e) {
+            return response()->json(["message" => "Hiányos adatok!"], 400, options:JSON_UNESCAPED_UNICODE);
+        }
+
     }
 }
